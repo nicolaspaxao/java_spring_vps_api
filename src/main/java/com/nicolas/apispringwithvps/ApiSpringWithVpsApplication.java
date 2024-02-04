9df17abe1a3af3ec15b06dd2951dc43704c57827
Nicolas Paxao <nicolaspaxao@gmail.com>
2024-02-04 20:16:33 -0300

    first commit

---
+++ src/main/java/com/nicolas/apispringwithvps/ApiSpringWithVpsApplication.java
@@ -0,0 +1,13 @@
+package com.nicolas.apispringwithvps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringWithVpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringWithVpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolas/apispringwithvps/ApiSpringWithVpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolas.apispringwithvps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringWithVpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
